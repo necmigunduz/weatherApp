2b1291fc77008742fda3f28b791bb7fdd0128bca
necmigunduz <necmigunduz@gmail.com>
2021-04-30 01:23:37 +0300

    Fix Eslint Errors

--- src/index.js
+++ src/index.js
@@ -1,54 +1,54 @@
-import _ from "lodash";
+import _ from 'lodash';
 
-const container = document.querySelector(".container");
+const container = document.querySelector('.container');
 
-const name = document.querySelector(".name");
-name.setAttribute("class", "text-center");
+const name = document.querySelector('.name');
+name.setAttribute('class', 'text-center');
 
-const description = document.querySelector(".description");
-description.setAttribute("class", "text-center");
+const description = document.querySelector('.description');
+description.setAttribute('class', 'text-center');
 
-const temp = document.querySelector(".temp");
-temp.setAttribute("class", "text-center");
+const temp = document.querySelector('.temp');
+temp.setAttribute('class', 'text-center');
 
-const humidity = document.querySelector(".humidity");
-humidity.setAttribute("class", "text-center");
+const humidity = document.querySelector('.humidity');
+humidity.setAttribute('class', 'text-center');
 
-const wind = document.querySelector(".wind");
-wind.setAttribute("class", "text-center");
+const wind = document.querySelector('.wind');
+wind.setAttribute('class', 'text-center');
 
-const visibility = document.querySelector(".visibility");
-visibility.setAttribute("class", "text-center");
+const visibility = document.querySelector('.visibility');
+visibility.setAttribute('class', 'text-center');
 
 function createForm() {
-  const header = document.createElement("h1");
-  header.setAttribute("class", "header bg-dark text-light text-center");
-  header.textContent = "WEATHER TODAY";
-  const form = document.createElement("form");
-  const label = document.createElement("label");
-  label.setAttribute("for", "cityname");
-  label.setAttribute("class", "cityname text-center mb-2");
-  label.setAttribute("aria-describedby", "cityname");
-  label.textContent = "Enter location name";
-  const input = document.createElement("input");
-  input.setAttribute("class", "city form-control");
-  input.setAttribute("id", "location");
-  const submit = document.createElement("button");
-  submit.textContent = "Submit";
-  submit.setAttribute("class", "btn btn-secondary mt-2 text-center w-100");
-  submit.setAttribute("type", "submit");
+  const header = document.createElement('h1');
+  header.setAttribute('class', 'header bg-dark text-light text-center');
+  header.textContent = 'WEATHER TODAY';
+  const form = document.createElement('form');
+  const label = document.createElement('label');
+  label.setAttribute('for', 'cityname');
+  label.setAttribute('class', 'cityname text-center mb-2');
+  label.setAttribute('aria-describedby', 'cityname');
+  label.textContent = 'Enter location name';
+  const input = document.createElement('input');
+  input.setAttribute('class', 'city form-control');
+  input.setAttribute('id', 'location');
+  const submit = document.createElement('button');
+  submit.textContent = 'Submit';
+  submit.setAttribute('class', 'btn btn-secondary mt-2 text-center w-100');
+  submit.setAttribute('type', 'submit');
 
-  submit.addEventListener("click", (e) => {
+  submit.addEventListener('click', (e) => {
     e.preventDefault();
 
     fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&APPID=8362038b9b4953c5bf68a1d7c665d255`,
-      { mode: "cors" }
+      { mode: 'cors' },
     )
-      .then(function (response) {
+      .then(function (response) { // disable-eslint-line
         return response.json();
       })
-      .then(function (data) {
+      .then(function (data) { // disable-eslint-line
         input.value = null;
 
         description.innerHTML = `${data.weather[0].description}`.toUpperCase();
